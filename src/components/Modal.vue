78f9239de1a1c8af02d5b4e16c6aa9b7029f4401
Ryan Wire <ryanwire@outlook.com>
2021-01-01 12:25:28 +0300

    Vertically centers svg icon inside button

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- Button trigger modal -->
     <button type="button" v-on:click="modalOpen = true" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#modal">
-      <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+      <svg style="width:24px;height:24px;margin-bottom:-5px;" viewBox="0 0 24 24">
         <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
       </svg>
     </button>
